c2f46c62eab284a713ea63f8f60233420f98dfc2
TalBM <talbenmeir@gmail.com>
2019-03-14 11:45:44 +0200

    more tenants updates

--- app/tenants/tenantsCtrl.js
+++ app/tenants/tenantsCtrl.js
@@ -1,6 +1,6 @@
 app.controller("tenantsCtrl", function ($scope, $log, $http) {
-    
-    function Tenant(familyName, heName, sheName ,floor, dira, isVaad, phone, email) {
+
+    function Tenant(familyName, heName, sheName, floor, dira, isVaad, phone, email) {
         this.familyName = familyName;
         this.heName = heName;
         this.sheName = sheName;
@@ -10,24 +10,33 @@ app.controller("tenantsCtrl", function ($scope, $log, $http) {
         this.phone = phone;
         this.email = email;
         this.show = true;
-        this.update=false;
+        this.update = false;
     }
 
 
     //----------- adding .json file to the array-------------
-    
-$scope.tenantsArray = [];
+
+    $scope.tenantsArray = [];
     $http.get("app/tenants/tenants_list.json").then(function (result) {
 
-        for(var i=0; i< result.data.length; i++){
-            var element=result.data[i];
-            element = new Tenant(element.familyName, element.heName, element.sheName ,element.floor, element.dira, element.isVaad, element.phone, element.email);
+        for (var i = 0; i < result.data.length; i++) {
+            var element = result.data[i];
+            element = new Tenant(
+                element.familyName,
+                element.heName,
+                element.sheName,
+                element.floor,
+                element.dira,
+                element.isVaad,
+                element.phone,
+                element.email
+            );
             $scope.tenantsArray.push(element);
-            }
-        
+        }
     })
 
-    $scope.deleteTenant =function(tenant){
+
+    $scope.deleteTenant = function (tenant) {
         tenant.show = false;
         // var pos=$scope.tenantsArray.indexOf(tenant);
         // if (pos>-1){
@@ -35,22 +44,27 @@ $scope.tenantsArray = [];
         // }
 
     }
-    
-    $scope.updateTenant=function(tenant){
-        tenant.update=!tenant.update;
+
+    $scope.updateTenant = function (tenant) {
+        tenant.update = !tenant.update;
         $scope.updateData = {};
     }
 
 
-    $scope.updateData = {};
-$scope.updateTenantSave=function(tenant){
+    $scope.updateData = {};//  כי המשתנים בפונקציה לא אותחלו
+    $scope.updateTenantSave = function (tenant) {
+        if (($scope.updateData.editedFamily === "") || ($scope.updateData.editedHe === "") || ($scope.updateData.editedShe === "")) {
+            return tenant;
+        }else {
     tenant.familyName = $scope.updateData.editedFamily;
     tenant.heName = $scope.updateData.editedHe;
     tenant.sheName = $scope.updateData.editedShe;
     tenant.isVaad = $scope.updateData.editedVaad;
     tenant.phone = $scope.updateData.editedPhone;
     tenant.email = $scope.updateData.editedEmail;
-    tenant.update=!tenant.update;
+    tenant.update = !tenant.update;
+}
+
     }
 
 });
